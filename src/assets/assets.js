8db318aa8817b43c70f48d7f30a28c0898c42bdf
fatma farouk <fatmafarouk170gemail.com>
2024-03-12 01:04:49 +0200

    jhdksjh

---
+++ src/assets/assets.js
@@ -0,0 +1,35 @@
+import history_icon from './history.png'
+import menu_icon from './meun.png'
+import plus_icon from './plus_icon.png'
+import question_icon from './help.png'
+import setting_icon from './setting.png'
+import bulb_icon from './bulb.png'
+import compass_icon from './compess.png'
+import gallery_icon from './addphoto.png'
+import mic_icon from './mic.png'
+import user_icon from './user_icon.png'
+import user from './user.jpg'
+import youtube_icon from './youtube_icon.png'
+import message_icon from './message.png'
+import code_icon from './code.png'
+import send_icon from './send.png'
+import gemini_icon from './gemini_icon.png'
+
+export const assets = {
+	history_icon,
+	menu_icon,
+	plus_icon,
+	question_icon,
+	setting_icon,
+	bulb_icon,
+	compass_icon,
+	gallery_icon,
+	mic_icon,
+	user_icon,
+	youtube_icon,
+	message_icon,
+	code_icon,
+	send_icon,
+	gemini_icon,
+	user,
+};
